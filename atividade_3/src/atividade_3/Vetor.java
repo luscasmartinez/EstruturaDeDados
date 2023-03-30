d6c29e7afe2280d76311d59919becf36915a8d1a
luscasmartinez <lucasmartinez.aluno@unipampa.edu.br>
2023-03-29 21:18:12 -0300

    atividade 3

---
+++ atividade_3/src/atividade_3/Vetor.java
@@ -0,0 +1,27 @@
+package atividade_3;
+
+public class Vetor {
+	
+	private double vetNotas [] = new double[5];
+	private Aluno vetAluno[] = new Aluno[5];
+	
+	
+	
+	public void addNota(double nota) {
+		if (vetNotas.length == 5) {
+	        for (int i = 0; i < vetNotas.length; i++) {
+	            if (vetNotas[i] == 0.0) {
+	            	vetNotas[i] = nota;
+	                break;
+	            }
+	        }
+	    } else {
+	        throw new RuntimeException("Vetor não tem tamanho suficiente para adicionar uma nova nota.");
+	    }
+	}
+	
+	public void addAluno() {
+		
+	}
+	
+}
---
+++ atividade_3/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Lucas
+ *
+ */
+module atividade_3 {
+}
