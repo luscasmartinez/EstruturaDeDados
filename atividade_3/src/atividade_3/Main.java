086559a287a8663e479399d420f43bccd92b7e8c
luscasmartinez <lucasmartinez.aluno@unipampa.edu.br>
2023-03-31 20:42:18 -0300

    20:42 31/03/2023

--- atividade_3/src/atividade_3/Main.java
+++ atividade_3/src/atividade_3/Main.java
@@ -5,6 +5,14 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		
+		Vetor turma = new Vetor(5);
+		Aluno aluno1 = new Aluno("João", 18, 8.5);
+		turma.adicionarAluno(aluno1);
+		Aluno aluno2 = new Aluno("Maria", 19, 7.8);
+		turma.adicionarAluno(aluno2);
+		// ...
+
 	}
 
 }
--- atividade_3/src/atividade_3/Vetor.java
+++ atividade_3/src/atividade_3/Vetor.java
@@ -1,31 +1,21 @@
 package atividade_3;
 
 public class Vetor {
-	
-	private double vetNotas [] = new double[5];
-	private Aluno vetAlunos[] = new Aluno[5];
-	
-	
-	public void addNota(double nota) {
-		if (vetNotas.length == 5) {
-	        for (int i = 0; i < vetNotas.length; i++) {
-	            if (vetNotas[i] == 0.0) {
-	            	vetNotas[i] = nota;
-	                break;
-	            }
-	        }
-	    } else {
-	        throw new RuntimeException("Vetor não tem tamanho suficiente para adicionar uma nova nota.");
-	    }
+
+	private Aluno[] alunos;
+	private int indice;
+
+	public Vetor(int tamanhoMaximo) {
+		alunos = new Aluno[tamanhoMaximo];
+		indice = 0;
 	}
-	
-	public void addAluno(Aluno aluno, Aluno[] vetAlunos) {
-		for (int i = 0; i < vetAlunos.length; i++) {
-			if(vetAlunos[i] == null) {
-				vetAlunos[i] = aluno;
-				break;
-			}
+
+	public void adicionarAluno(Aluno aluno) {
+		if (indice < alunos.length) {
+			alunos[indice] = aluno;
+			indice++;
+		} else {
+			System.out.println("Turma cheia!");
 		}
 	}
-	
 }
