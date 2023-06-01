408fbb4ca3ec391d2a73057f4b5393f626fbbfa7
Lucas de Lima Martinez <lucasmartinez.aluno@unipampa.edu.br>
2023-06-01 15:54:06 -0300

    15:53 02/06/2023

--- 13 - Atividade/Main.java
+++ 13 - Atividade/Main.java
@@ -1,6 +1,23 @@
 public class Main {
     public static void main(String[] args) {
-        PilhaLista pilhaLista = new PilhaLista();
-        PilhaVet pilhaVet = new PilhaVet(3);
+        FilaAlunos fila = new FilaAlunos();
+        PilhaVet pilha = new PilhaVet(10);
+
+        // Adicione alguns alunos à fila
+        fila.enfileirar(new Aluno("João", 20, 80));
+        fila.enfileirar(new Aluno("Maria", 19, 90));
+        fila.enfileirar(new Aluno("Pedro", 21, 85));
+
+        // Inverta a ordem dos alunos usando a pilha
+        while (!fila.isEmpty()) {
+            Aluno aluno = fila.desenfileirar();
+            pilha.push(aluno);
+        }
+
+        // Agora, a pilha contém os alunos invertidos
+        while (!pilha.isEmpty()) {
+            Aluno aluno = (Aluno) pilha.pop();
+            System.out.println(aluno.getNome());
+        }
     }
 }
